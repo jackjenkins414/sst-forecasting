10103b8bb08da9afbf808f61f61bc593f25e8b35
Isaac Jaensch <u7262835@anu.edu.au>
2026-05-02 18:11:03 +1000

    Helper function for initialising the hidden states.

--- src/models/lstm.py
+++ src/models/lstm.py
@@ -13,13 +13,14 @@ Written by Isaac Jaensch (u7262835), ANU, 2026.
 
 import torch
 import torch.nn as nn
+import numpy as np
 
 class SST_LSTM(nn.Module):
 
     def __init__(self, input_dim, hidden_dim, output_dim, num_layers):
         super.__init__()
 
-        self.H = hidden_dim
+        self.hidden_dim = hidden_dim
         self.num_layers = num_layers
 
         self.lstm = nn.LSTM(
@@ -36,11 +37,8 @@ class SST_LSTM(nn.Module):
         predictions = self.fc(out)
         return predictions, hidden
     
+    # Initialises the hidden state for the LSTM. 
     def init_hidden(self, batch_size, device):
-        
-
-
-
-
-
-    
+        h0 = torch.zeros(self.num_layers, batch_size, self.hidden_dim, device=device)
+        c0 = torch.zeros(self.num_layers, batch_size, self.hidden_dim, device=device)
+        return (h0, c0)
