e43906fe21b67e5e45fafeff6b2983b2a27afd26
Isaac Jaensch <u7262835@anu.edu.au>
2026-05-02 16:36:09 +1000

    LSTM file header for lstm/isaac branch

---
+++ src/models/lstm.py
@@ -0,0 +1,14 @@
+"""
+A basic demo LSTM model, created as a precursor to the actual data-aligned
+models for the COMP3242 Group Research Project. 
+
+Please note that this model was made purely as a proof-of-concept for the 
+Week 9 meeting, and is unlikely to be viable as an assignment model.
+
+Inspiration taken from:
+    N/A
+
+Written by Isaac Jaensch (u7262835), ANU, 2026. 
+"""
+
+# TODO: Create the outline of an LSTM model, and some basic skeleton code.
