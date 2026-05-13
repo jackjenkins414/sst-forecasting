669b6cea756cc8fec479bfeabfecfad563d6b52f
Isaac Jaensch <u7262835@anu.edu.au>
2026-05-13 22:16:04 +1000

    Created the framework for the ProbSparse Attention Informer model.

---
+++ src/models/informer.py
@@ -0,0 +1,148 @@
+# TODO: This currently is only a framework. Code and comments will be added 
+#       in due course. 
+
+import torch
+import torch.nn as nn
+
+#TODO
+class SpatialProjection(nn.Module):
+    def __init__():
+        #TODO
+        return
+    
+    def forward(self, x):
+        #TODO
+        return
+    
+#TODO
+class PositionalEncoding(nn.Module):
+    def __init__():
+        #TODO
+        return
+    
+    def forward(self, x):
+        #TODO
+        return
+    
+#TODO
+class ProbSparseAttention(nn.Module):
+    def __init__():
+        #TODO
+        return
+    
+    def forward(self, x):
+        #TODO
+        return
+    
+    def prob_QK():
+        #TODO
+        return
+    
+    def get_initial_context():
+        #TODO
+        return
+    
+    def update_context():
+        #TODO
+        return
+    
+#TODO
+class AttentionLayer(nn.Module):
+    def __init__():
+        #TODO
+        return
+    
+    def forward(self, x):
+        #TODO
+        return
+    
+#TODO
+class FeedForwardBlock(nn.Module):
+    def __init__():
+        #TODO
+        return
+    
+    def forward(self, x):
+        #TODO
+        return
+    
+#TODO
+class EncoderLayer(nn.Module):
+    def __init__():
+        #TODO
+        return
+    
+    def forward(self, x):
+        #TODO
+        return
+    
+#TODO
+class InformerEncoder(nn.Module):
+    def __init__():
+        #TODO
+        return
+    
+    def forward(self, x):
+        #TODO
+        return
+    
+#TODO
+class DecoderLayer(nn.Module):
+    def __init__():
+        #TODO
+        return
+    
+    def forward(self, x):
+        #TODO
+        return
+    
+#TODO
+class InformerDecoder(nn.Module):
+    def __init__():
+        #TODO
+        return
+    
+    def forward(self, x):
+        #TODO
+        return
+    
+#TODO
+class EncoderLayer(nn.Module):
+    def __init__():
+        #TODO
+        return
+    
+    def forward(self, x):
+        #TODO
+        return
+    
+#TODO
+class ProjectionHead(nn.Module):
+    def __init__():
+        #TODO
+        return
+    
+    def forward(self, x):
+        #TODO
+        return
+    
+#TODO
+class EncoderLayer(nn.Module):
+    def __init__():
+        #TODO
+        return
+    
+    def forward(self, x):
+        #TODO
+        return
+
+  
+#TODO
+class ProbSparseInformer(nn.Module):
+    def __init__():
+        #TODO
+        return
+    
+    def forward(self, x):
+        #TODO
+        return
